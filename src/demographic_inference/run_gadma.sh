3fc709857e6672ac2bc672c452fe7fd99bcbe39f
Enrico Bazzicalupo <enricobazzical@gmail.com>
2024-05-29 18:08:16 +0200

    added folder for resuming runs

--- src/demographic_inference/run_gadma.sh
+++ src/demographic_inference/run_gadma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 10
-#SBATCH --mem=20G
-#SBATCH -t 3-00:00:00
+#SBATCH --mem=40G
+#SBATCH -t 03-00:00:00
 
 # load the necessary modules
 module load cesga/system miniconda3/22.11.1-1
@@ -9,9 +9,14 @@ source activate /mnt/netapp1/Store_CSIC/home/csic/eye/eba/gadma2
 
 cd data/demographic_inference
 
-# define the pair and the iteration number
+# define the pair, the iteration number and the folder
 pair=$1
 n=$2
+folder=$3
 
 # run gadma
-gadma -p ${pair}.gadma.parameters -o ${pair}_${n}
+if [ -d ${folder} ]; then
+    gadma --resume ${folder}
+else
+    gadma -p ${pair}.gadma.parameters -o ${pair}_${n}
+fi
