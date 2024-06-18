147920a75179f04621620258b7c1b8760f87f9ed
Enrico Bazzicalupo <enricobazzical@gmail.com>
2024-06-18 16:26:14 +0200

    from gadma results to CI table

---
+++ src/demographic_inference/dadi_optimization_boot.py
@@ -0,0 +1,135 @@
+import gadma2_best_dadi_models as models
+import dadi
+import argparse
+import os
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--boot', type=str, required=True)
+    parser.add_argument('--model', type=str, required=True)
+    parser.add_argument('--pop_pair', type=str, required=True)
+    parser.add_argument('--csv', type=str, required=True)
+    return parser.parse_args()
+
+def optimization_hyperparams():
+    # The number of grid points to use in the optimization
+    pts = [50, 60, 70]
+    # The maximum number of iterations for the optimization
+    maxiter = 20
+    return pts, maxiter
+
+def get_model(model_name: str):
+    try:
+        return dadi.Numerics.make_extrap_log_func(getattr(models, f"model_func_{model_name}"))
+    except AttributeError:
+        raise ValueError(f"Model {model_name} not found in gadma2_best_dadi_models.py")
+
+def get_boot_file(filename: str):
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"File {filename} not found")
+    else:
+        return filename
+
+def get_popids(pop_pair: str):
+    popids = pop_pair.split('-')
+    popids.reverse()
+    return popids
+
+def get_projections(pop_pair: str):
+    if pop_pair == 'lpa-wel':
+        return [40, 44]
+    elif pop_pair == 'lpa-sel':
+        return [24, 44]
+    elif pop_pair == 'lpa-eel':
+        return [38, 44]
+    else:
+        raise ValueError(f"Projections for population pair {pop_pair} not found")
+    
+def get_data(boot:str):
+    data = dadi.Spectrum.from_file(boot)
+    return data
+
+def get_p0(model_name: str):
+    try:
+        return getattr(models, f"p0_{model_name}")()
+    except AttributeError:
+        raise ValueError(f"Model {model_name} not found in gadma2_best_dadi_models.py")
+
+def get_pnames(model_name: str):
+    try:
+        return getattr(models, f"pnames_{model_name}")()
+    except AttributeError:
+        raise ValueError(f"Model {model_name} not found in gadma2_best_dadi_models.py")
+
+def main() -> None:
+    # read arguments
+    args = parse_arguments()
+    print(f"Optimizing model {args.model} for population pair {args.pop_pair}\n")
+    
+    # add log file
+    log_file = args.csv
+    print(f'Log file: {log_file}')
+
+    # read parameter names
+    pnames = get_pnames(args.model)
+    pnames_str = ','.join(pnames)
+
+    # write header to log file
+    with open(log_file, 'w') as f:
+        f.write(f"pop_pair,model,log-likelihood,{pnames_str}\n")
+    
+    # read data from vcf
+    print("Reading data...")
+    data = get_data(args.boot)
+    
+    # get model function
+    model_func = get_model(args.model)
+    
+    # get optimization hyperparameters
+    pts, maxiter = optimization_hyperparams()
+    print(f'Optimization hyperparameters: pts={pts}, maxiter={maxiter}')
+    
+    # get model's initial parameters
+    p0 = get_p0(args.model)
+    print(f'Initial parameters: {p0}')
+    
+    # sim model with initial parameters
+    model = model_func(p0, data.sample_sizes, pts)
+    
+    # get initial log-likelihood
+    ll_model = dadi.Inference.ll_multinom(model, data)
+    print(f'Initial log-likelihood: {ll_model}')
+    
+    # write initial conditions to log file
+    with open(log_file, 'a') as f:
+        f.write(f"{args.pop_pair},{args.model},{ll_model},{','.join(str(val) for val in p0)}\n")
+
+    # run optimization
+    print(f'Optimization of {args.boot}')
+    
+    # bound initial parameters
+    fixed_p = [p if p == 0.0 else None for p in p0]
+    lower_bound = [p * 0.1 for p in p0]
+    upper_bound = [p * 10 for p in p0]
+    
+    # optimize
+    opt_params = dadi.Inference.optimize_log(
+        p0 = p0, data = data,
+        model_func = model_func,
+        lower_bound=lower_bound, upper_bound=upper_bound, fixed_params=fixed_p,
+        maxiter=maxiter, pts = pts, verbose=1
+        )
+    
+    # calculate log-likelihood
+    model = model_func(opt_params, data.sample_sizes, pts)
+    new_ll_model = dadi.Inference.ll_multinom(model, data)
+    print(f'optimization {args.boot} log-likelihood: {new_ll_model}')
+    print(f'optimization {args.boot} parameters: {opt_params}')
+    
+    # write results to log file
+    with open(log_file, 'a') as f:
+        f.write(f"{args.pop_pair},{args.model},{new_ll_model},{','.join(str(val) for val in opt_params.tolist())}\n")
+    
+    return
+if __name__ == '__main__':
+    main()
---
+++ src/demographic_inference/make_block_bootstrap.py
@@ -0,0 +1,46 @@
+import dadi
+import argparse
+
+def parse_args():
+    """
+    Parse command line arguments: vcf, popmap, out_dir, n_bootstraps
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vcf", type=str, help="VCF file", required=True)
+    parser.add_argument("--popmap", type=str, help="PopMap file", required=True)
+    parser.add_argument("--pop_pair", type=str, help="Population Pair: (lpa-wel, lpa-sel, lpa-eel)", required=True)
+    parser.add_argument("--out_dir", type=str, help="Output directory", required=True)
+    parser.add_argument("--n_bootstraps", type=int, help="Number of bootstraps", default=100)
+    parser.add_argument("--chunk_size", type=int, help="Chunk size", default=200_000)
+    return parser.parse_args()
+
+def main(vcf, popmap, pop_pair, out_dir, n_bootstraps, chunk_size):
+    """
+    Create n_bootstraps of the VCF file
+    """
+    pop_ids = pop_pair.split("-")
+    pop_ids.reverse()
+    dd = dadi.Misc.make_data_dict_vcf(
+        vcf_filename = vcf, popinfo_filename = popmap, filter=True, flanking_info=[None, None]
+        )
+    chunks = dadi.Misc.fragment_data_dict(dd, chunk_size)
+    if pop_pair == "lpa-wel":
+        ns = [40, 44]
+    elif pop_pair == "lpa-sel":
+        ns = [38, 44]
+    elif pop_pair == "lpa-eel":
+        ns = [24, 44]
+    boots = dadi.Misc.bootstraps_from_dd_chunks(chunks, n_bootstraps, pop_ids, ns, polarized = False)
+    for n in range(len(boots)):
+        boots[n].to_file(f'{out_dir}/{pop_pair}_{n}.fs')
+    return
+
+if __name__ == "__main__":
+    args = parse_args()
+    print(f"VCF file: {args.vcf}")
+    print(f"PopMap file: {args.popmap}")
+    print(f"Populations IDs: {args.pop_pair}")
+    print(f"Output directory: {args.out_dir}")
+    print(f"Number of bootstraps: {args.n_bootstraps}")
+    print(f"Chunk size: {args.chunk_size}")
+    main(args.vcf, args.popmap, args.pop_pair, args.out_dir, args.n_bootstraps, args.chunk_size)
